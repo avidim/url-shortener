dec1bb232212c19337788ea034a911685799433e
Javid <javidgasimzada@gmail.com>
2022-01-20 17:17:31 +0400

    add routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('form');
-});
+Route::get('/', function () { return view('form'); });
+Route::post('short', [ ShortLinkController::class, 'short' ]);
+Route::get('{shortLink}', [ ShortLinkController::class, 'goTo' ]);
